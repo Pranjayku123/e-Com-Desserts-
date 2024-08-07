6c4971734d24a12fc30998f8907fc143b5b834c5
PRANJAY KUMAR <pranjayku123@gmail.com>
2024-08-07 19:01:41 +0530

    test

--- src/script.js
+++ src/script.js
@@ -1,18 +1,29 @@
 async function getPeople() {
-  let response = await fetch("../data.json");
-  let data = await response.json();
-  console.log(data);
-  let cardDesign = `
-    <div class="cardDesign">
+    try {
+      let response = await fetch("../data.json");
+      let data = await response.json();
+      console.log(data);
+      
+      let cardDesigns = data.map(item => `
+        <div class="cardDesign">
           <div class="innerImgCard">
-            <img src="" alt="" id="dessertImageId">
+            <img src="${item.image.desktop}" alt="${item.name}" id="dessertImageId">
           </div>
-          <div class="dessertsName"></div>
-          <div class="dessertFullName"></div>
-          <div class="price"></div>
+          <div class="itemDiscription">
+          <div class="dessertsName">${item.category}</div>
+          <div class="dessertFullName">${item.name}</div>
+          <div class="price">$${item.price.toFixed(2)}</div>
+          </div>
         </div>
-    `;
-
-    
-}
-getPeople();
+      `).join('');
+  
+      let container = document.getElementById("contentMainDivId");
+      container.innerHTML = cardDesigns;
+  
+    } catch (error) {
+      console.error("Error fetching and displaying data:", error);
+    }
+  }
+  
+  getPeople();
+  
